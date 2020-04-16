09909abd6117dadde3cd274b85ae436f1ed8e8a8
sabbir421 <56802016+sabbir421@users.noreply.github.com>
2020-04-16 23:37:53 +0600

    Add files via upload

---
+++ function.py
@@ -0,0 +1,15 @@
+list=[1,2,3,4,5,8,1,9,10,12]
+list.append(7)
+print(list)
+list.remove(5)
+print(list)
+index=3
+list.insert(index,14)
+print(list)
+list.remove(max(list))
+print(list)
+list.remove(min(list))
+print(list)
+list.sort()
+print(list)
+
