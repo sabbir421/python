bc9228b361015a7df5689e04078992ca47fc8b62
sabbir421 <56802016+sabbir421@users.noreply.github.com>
2020-04-25 19:34:02 +0600

    Add files via upload

---
+++ number of month.py
@@ -0,0 +1,36 @@
+num=int(input("number of month :"))
+if num ==1:
+    print("January")
+else:
+    if num ==2:
+        print("February")
+    else:
+        if num ==3:
+            print("March")
+        else:
+            if num==4:
+                print("April")
+            else:
+                if num ==5:
+                    print("May")
+                else:
+                    if num ==6:
+                        print("June")
+                    else:
+                        if num ==7:
+                            print("July")
+                        else:
+                            if num==8:
+                                print("August")
+                            else:
+                                if num ==9:
+                                    print("September")   
+                                else:
+                                    if num ==10:
+                                        print("October")
+                                    else:
+                                        if num==11:
+                                            print("November")
+                                        else:
+                                            if num ==12:
+                                                print("December ")
