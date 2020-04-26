ddd0713d18885975f2b746986f90a2c1e81286ed
sabbir421 <56802016+sabbir421@users.noreply.github.com>
2020-04-26 23:51:28 +0600

    Add files via upload

---
+++ if elif else.py
@@ -0,0 +1,17 @@
+day=int(input("number of day: "))
+if day==1:
+    print("Saturday")
+elif day ==2:
+    print("Sunday")
+elif day ==3:
+    print("Monday ")
+elif day ==4:
+    print("Tuesday ")
+elif day ==5:
+    print("Wednesday ")
+elif day ==6:
+    print("Tuesday")
+elif day ==7:
+    print("Friday ")
+else:
+    print("7 days in week")
